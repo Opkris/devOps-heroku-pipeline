373829631ada8d7c2bd4672ab6e787949f2ec81a
OpKris <kristofferopdahl@mail.com>
2020-09-07 09:31:36 +0200

    added demoApplicationTest

---
+++ src/test/java/demo/demoApplicationTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class demoApplicationTest {
+    @SpringBootTest
+    class DemoApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+}
