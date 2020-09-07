0ce42115c159c4118e1943ec5057902b47662fb5
OpKris <kristofferopdahl@mail.com>
2020-09-07 09:41:35 +0200

    added a new test

--- src/test/java/demo/demoApplicationTest.java
+++ src/test/java/demo/demoApplicationTest.java
@@ -3,11 +3,23 @@ package demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class demoApplicationTest {
 
         @Test
         void contextLoads() {
+        }
+
+        @Test
+    public void simpleTest(){
+            int a = 5;
+            int b = 10;
+            int c =  a + b;
+
+            assertEquals(15, c);
+
         }
 
     }
